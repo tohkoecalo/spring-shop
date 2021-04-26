5eb6a4e451bfca4922dfd81ff3667b237fc5542d
tohkoecalo <tohkoecalo@gmail.com>
2021-04-26 21:20:05 +0500

    fetching products from db implemented

---
+++ src/main/java/com/testproject/shop/Product.java
@@ -0,0 +1,51 @@
+package com.testproject.shop;
+
+import javax.persistence.*;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+
+    private int id;
+    private String name;
+    private String description;
+    private int price;
+
+    public Product(){}
+
+    public Product(int id, String name, String description, int price){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public void setPrice(int price){
+        this.price = price;
+    }
+}
